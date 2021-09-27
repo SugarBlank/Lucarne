d1558b1a4b5bf0712b1d5ad023657827c46a579e
scouri <eneblank19@protonmail.com>
2021-09-26 22:24:43 -0400

    Add files via upload

---
+++ app/src/main/java/app/olauncher/data/Constants.kt
@@ -0,0 +1,25 @@
+package app.olauncher.data
+
+object Constants {
+    const val THEME_MODE_DARK = 0
+    const val THEME_MODE_LIGHT = 1
+
+    const val FLAG_LAUNCH_APP = 100
+    const val FLAG_HIDDEN_APPS = 101
+
+    const val FLAG_SET_HOME_APP_1 = 1
+    const val FLAG_SET_HOME_APP_2 = 2
+    const val FLAG_SET_HOME_APP_3 = 3
+    const val FLAG_SET_HOME_APP_4 = 4
+    const val FLAG_SET_HOME_APP_5 = 5
+    const val FLAG_SET_HOME_APP_6 = 6
+    const val FLAG_SET_HOME_APP_7 = 7
+    const val FLAG_SET_HOME_APP_8 = 8
+
+    const val FLAG_SET_SWIPE_LEFT_APP = 11
+    const val FLAG_SET_SWIPE_RIGHT_APP = 12
+    const val FLAG_SET_DOUBLE_TAP_APP = 13
+
+    const val TRIPLE_TAP_DELAY_MS = 300
+    const val LONG_PRESS_DELAY_MS = 600
+}
